4e8244215b26aed809bfcb7f0a382f1416be6f82
OmegaOckie <ockiejnr@gmail.com>
2022-10-21 14:50:08 +0200

    Failed Attempt

--- GADE 7312 POE/Main.cpp
+++ GADE 7312 POE/Main.cpp
@@ -220,6 +220,33 @@ int main()
 	//BIND VAO 0
 	glBindVertexArray(0);
 
+	 //Texture INIT
+	int image_width = 0;
+	int image_height = 0;
+	unsigned char* image = SOIL_load_image("Images/PolarBear.png", &image_width, &image_height, NULL, SOIL_LOAD_RGBA);
+
+	GLuint texture0;
+	glGenTextures(1, & texture0);
+	glBindTexture(GL_TEXTURE_2D, texture0);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+	if (image)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0 , GL_RGBA, GL_UNSIGNED_BYTE, image);
+			glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "ERROR::TEXTURE_LOADING_FAILED" << "\n";
+	}
+
+	glActiveTexture(0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	SOIL_free_image_data(image);
 	 
 	//MAIN  LOOP
 	while (!glfwWindowShouldClose(window))
